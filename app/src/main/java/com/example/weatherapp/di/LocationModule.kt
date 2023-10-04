97a185032ab3b5cb2c0e6bc1449f48a71c8a9315
aljoharash <aljoharash76@gmail.com>
2023-10-04 23:40:44 +0300

    get user's location

---
+++ app/src/main/java/com/example/weatherapp/di/LocationModule.kt
@@ -0,0 +1,48 @@
+package com.example.weatherapp.di
+
+import android.app.Application
+import android.content.Context
+import com.example.weatherapp.data.location.DefaultLocationTracker
+import com.example.weatherapp.domain.location.LocationTracker
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+object LocationModule {
+
+    @Provides
+    @Singleton
+    fun provideFusedLocationProviderClient(app: Application): FusedLocationProviderClient {
+        return LocationServices.getFusedLocationProviderClient(app)
+    }
+
+    @Provides
+    @Singleton
+    fun providesLocationTracker(
+        fusedLocationProviderClient: FusedLocationProviderClient ,
+        application: Application
+    ): LocationTracker = DefaultLocationTracker(
+        fusedLocationProviderClient ,
+        application
+    )
+
+    @Provides
+    @Singleton
+    fun provideApplicationContext(
+        application: Application
+    ): Context {
+        return application
+    }
+
+
+}
+
+
