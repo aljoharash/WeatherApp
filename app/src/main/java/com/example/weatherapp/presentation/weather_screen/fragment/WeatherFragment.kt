304dc23dedceea9c9b738c834fc9ceaf7d5c03db
aljoharash <aljoharash76@gmail.com>
2023-10-05 04:52:17 +0300

    UI

--- app/src/main/java/com/example/weatherapp/presentation/weather_screen/fragment/WeatherFragment.kt
+++ app/src/main/java/com/example/weatherapp/presentation/weather_screen/fragment/WeatherFragment.kt
@@ -1,7 +1,56 @@
 package com.example.weatherapp.presentation.weather_screen.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.weatherapp.databinding.FragmentWeatherBinding
+import com.example.weatherapp.presentation.weather_screen.viewmodel.WeatherViewModel
+import kotlinx.coroutines.launch
 
 class WeatherFragment : Fragment() {
+
+    private var binding: FragmentWeatherBinding?= null
+    private val weatherViewModel: WeatherViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater , container: ViewGroup? ,
+        savedInstanceState: Bundle?
+    ): View {
+        val fragmentBinding = FragmentWeatherBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+
+
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            weatherFragment = this@WeatherFragment
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            weatherViewModel.state.collect { state ->
+                state.let {
+                }
+            }
+        }
+    }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+
+
 
 }
