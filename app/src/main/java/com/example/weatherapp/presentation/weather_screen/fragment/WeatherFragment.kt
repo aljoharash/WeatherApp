2e04b1e0dc6b7e315d63ce8764be3534758a429c
aljoharash <aljoharash76@gmail.com>
2023-10-06 04:12:22 +0300

    UI + weatherInfo viewBinding

--- app/src/main/java/com/example/weatherapp/presentation/weather_screen/fragment/WeatherFragment.kt
+++ app/src/main/java/com/example/weatherapp/presentation/weather_screen/fragment/WeatherFragment.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp.presentation.weather_screen.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,20 +8,22 @@ import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentWeatherBinding
+import com.example.weatherapp.presentation.weather_screen.viewmodel.WeatherState
 import com.example.weatherapp.presentation.weather_screen.viewmodel.WeatherViewModel
 import kotlinx.coroutines.launch
 
 class WeatherFragment : Fragment() {
 
-    private var binding: FragmentWeatherBinding?= null
+    private var binding: FragmentWeatherBinding? = null
     private val weatherViewModel: WeatherViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
         savedInstanceState: Bundle?
     ): View {
-        val fragmentBinding = FragmentWeatherBinding.inflate(inflater, container, false)
+        val fragmentBinding = FragmentWeatherBinding.inflate(inflater , container , false)
         binding = fragmentBinding
 
 
@@ -28,21 +31,16 @@ class WeatherFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding?.apply {
-            lifecycleOwner = viewLifecycleOwner
-            weatherFragment = this@WeatherFragment
-        }
+        super.onViewCreated(view , savedInstanceState)
 
         viewLifecycleOwner.lifecycleScope.launch {
             weatherViewModel.state.collect { state ->
                 state.let {
+                    setWeatherInfo()
                 }
             }
         }
     }
-
 
 
     override fun onDestroyView() {
@@ -50,7 +48,28 @@ class WeatherFragment : Fragment() {
         binding = null
     }
 
+    private fun setWeatherInfo() {
 
+        binding?.city?.text = weatherViewModel.state.value.city
+
+        binding?.degreeText?.text = getString(
+            R.string.temp_degree, weatherViewModel.state.value.weatherInfo?.current?.temp.toString()
+        )
+
+        binding?.weatherDescription?.text =
+            weatherViewModel.state.value.weatherInfo?.current?.weather?.get(0)?.description
+
+        binding?.humidityValue?.text =
+            weatherViewModel.state.value.weatherInfo?.current?.humidity.toString()
+
+        binding?.windValue?.text =
+            weatherViewModel.state.value.weatherInfo?.current?.wind_speed.toString()
+
+        binding?.feelsLikeValue?.text =
+            weatherViewModel.state.value.weatherInfo?.current?.feels_like.toString()
+
+
+    }
 
 
 }
--- app/src/main/java/com/example/weatherapp/presentation/weather_screen/viewmodel/WeatherEvent.kt
+++
@@ -1,5 +0,0 @@
-package com.example.weatherapp.presentation.weather_screen.viewmodel
-
-sealed class WeatherEvent{
-    object GetWeather: WeatherEvent()
-}
